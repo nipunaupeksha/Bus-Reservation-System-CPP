763c5f42023452238e8ab54e0cc4b3ade72ea96a
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-07-10 11:01:22 +0530

    added some snippets

--- Bus Reservation System/main.cpp
+++ Bus Reservation System/main.cpp
@@ -1,6 +1,36 @@
+#include <conio.h>
+
+#include <cstdio>
+
 #include <iostream>
 
+#include <string.h>
+
+#include <cstdlib>
+
 using namespace std;
+static int p = 0;
+class a
+{
+char busn[5], driver[10], arrival[5], depart[5], from[10], to[10], seat[8][4][10];
+public:
+  void install();
+  void allotment();
+  void empty();
+  void show();
+  void avail();
+  void position(int i);
+
+}
+bus[10];
+
+void vline(char ch)
+{
+  for (int i=80;i>0;i--)
+  cout<<ch;
+
+}
+
 
 int main()
 {
